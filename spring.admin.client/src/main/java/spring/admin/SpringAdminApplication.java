c85f94f9cf3c7a9d92573c036ba9e602f7f83031
jeremie <jeremie@dell5567>
2020-09-13 13:35:11 +0200

    init

--- spring.admin.client/src/main/java/spring/admin/SpringAdminApplication.java
+++ spring.admin.client/src/main/java/spring/admin/SpringAdminApplication.java
@@ -3,9 +3,6 @@ package spring.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
-@EnableAdminServer
 @SpringBootApplication
 public class SpringAdminApplication {
 
